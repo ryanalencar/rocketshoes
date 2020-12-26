f34e53a80ff2a6dfe13bd7f0becd2f1c3e1ae088
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-12-26 17:57:24 -0300

    feat: husky and lint-staged config

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import { MdShoppingBasket } from 'react-icons/md'
+
+import { Container, Cart } from './styles'
+import logo from '../../assets/images/logo.svg'
+
+export default function Header() {
+  console.log(logo)
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Logo RocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  )
+}
