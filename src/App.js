bcb1febeefa8261330ee4ce6e1aeb241cd3b0728
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2021-01-08 12:50:21 -0300

    feat: config redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import Routes from './routes'
 import Header from './components/Header'
 import GlobalStyle from './styles/global'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   )
 }
